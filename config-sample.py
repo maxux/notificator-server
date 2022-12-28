b043c296c4b2a6f4b9a6c21e3dbd91c5ae918de0
Maxime Daniel <root@maxux.net>
2022-12-28 23:47:23 +0100

    config: add sample code

---
+++ config-sample.py
@@ -0,0 +1,16 @@
+
+config = {
+    'db-server': '127.0.0.1',
+    'db-user': 'notify',
+    'db-password': '',
+    'db-dbname': 'notify',
+
+    'http-port': 2000,
+    'http-secret': '',
+    'http-domain': 'https://notify.example.com',
+
+    'redis-host': '127.0.0.1',
+    'redis-queue': 'notificator',
+
+    'debug': True,
+}
